d1191d5a2a1c2f6470004864efdaf8e156993eab
pjy96 <doooooo02@kakao.com>
2022-07-12 16:27:00 +0900

    재택

--- myScript.js
+++ myScript.js
@@ -17,27 +17,62 @@ function rClose(){
 function printResult(inputbar){
 
     var inputbar = document.getElementById("inputbar").value; /* 입력 받은 내용*/
-    var result = document.getElementById("result"); /* 검색결과 출력*/
+    var result = document.getElementById("result"); /* 검색결과 출력창*/
     var tc = document.getElementById("rClose"); /* 쓰레기통 모양 아이콘 */
     var element = document.getElementById("time"); /* div time (현재시간) */
 
-    const regIp = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/; 
     /* IP 정규식 : 0~255사이의 숫자 + "." + 0~255사이의 숫자 + "." + 0~255사이의 숫자 + "." 0~255사이의 숫자 */
-    const regEmail = /^\w+@\w+\.[0-9a-zA-Z]+$/;
+    const regIp = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/; 
     /* Email 정규식 : \w(=[0-9a-zA-Z_]) 1회이상 @ \w 1회이상 "." 숫자&알파벳 */
+    const regEmail = /^\w+@\w+\.[0-9a-zA-Z]+$/;
+
+
+    var addList = document.createElement("p"); // 새로운 태그를 마지막 자식노드로 추가
+    var textnode = document.createTextNode(element.innerText + " | " + inputbar); //텍스트 생성
 
     if(window.event.keyCode == 13){ /* enter's ascii code number = 13 */
 
+        addList.appendChild(textnode);
+        result.innerText = addList.textContent;
+
         if(regIp.test(inputbar)){ /* regIp와 input 값 비교 */
-            result.innerText = element.innerText + " | " + inputbar + " is Email";
+
+            addList.append(element.innerText + " | " + inputbar + " is IP");
+            result.innerText = addList.textContent;
+
         }else if(regEmail.test(inputbar)){ /* regEmail과 input 값 비교 */
             result.innerText = element.innerText + " | " + inputbar + " is Email";
         }else{ /* 유효하지 않은 형식입니다 */
             result.innerText = element.innerText + " | " + inputbar + " is invalid format";
         }
+
         tc.disabled = false; /* 쓰레기통 모양 아이콘 활성화 */
+
     }
 }
+
+function test(){
+
+    var element = document.getElementById("time"); /* div time (현재시간) */
+    var newDiv = document.createElement('div'); // div element 만들기
+    var newText = document.createTextNode(element.innerText); // div에 들어갈 textnode 만들기
+
+    newDiv.appendChild(newText); // div에 textnode 붙이기
+    Container.appendChild(newDiv); // 결과 출력창에 div element 붙이기
+    console.log(element.innerText)
+    // 여기까진 element appendChild
+
+    // var arr = [5]; // 배열선언
+    // var entire = arr.push(element); // 배열에 element 추가
+
+    // console.log(arr)
+    // console.log(entire)
+
+
+}
+
+
+
 
 /* fetch로 좌상단 서버타임 출력하기 */
 // setInterval(function serverTime(){ 
