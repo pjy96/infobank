b9ccbe40e51db85d134d150a51945738e89861e2
pjy96 <doooooo02@kakao.com>
2022-07-12 14:11:37 +0900

    ajax

--- myScript.js
+++ myScript.js
@@ -1,10 +1,9 @@
 /* xmark function */
 function fClose(){
     var del = document.getElementById('inputbar');
-
     del.value = ""; /* 검색바 내용 삭제 */
-
 }
+
 /* 쓰레기통 모양 아이콘 기능 : 출력 내용 삭제 */
 function rClose(){ 
     var delt = document.getElementById('result');
@@ -12,30 +11,7 @@ function rClose(){
  
     delt.innerHTML = ""; /* 출력 내용 삭제 */
     tc.disabled = true; /* 아이콘 비활성화 */
-
-    //var value = arr.remove(), arr.shift()/arr.pop() ;
-    //delete arr[몇번째];
-
 }
-/* 좌상단 서버타임 출력하기 */
-setInterval(function serverTime(){ 
-
-    const config = {
-        method: "get"
-    }; /* GET request */
-
-    fetch("http://worldtimeapi.org/api/timezone/Asia/Seoul", config) /* url 주소로 get 요청 보내기 */
-        .then(response => response.json())    /* json 형식으로 응답받음 */
-        .then(data => {
-            const time = document.getElementById("time"); /* time 출력창 */
-            var getTime = data.datetime;
-            var setDay = getTime.substring(0,10); /* 전체 문자열 중 날짜에 해당 */
-            var setTime = getTime.substring(11,19); /* 전체 문자열 중 시간에 해당 */
-            time.innerText = setDay + " " + setTime; /* 최종 time 출력 */
-    })
-    .catch(error => console.log(error)); /* error 발생시 */
-
-},1000);
 
 /* after enter, run reg, print result */
 function printResult(inputbar){
@@ -50,7 +26,6 @@ function printResult(inputbar){
     const regEmail = /^\w+@\w+\.[0-9a-zA-Z]+$/;
     /* Email 정규식 : \w(=[0-9a-zA-Z_]) 1회이상 @ \w 1회이상 "." 숫자&알파벳 */
 
-
     if(window.event.keyCode == 13){ /* enter's ascii code number = 13 */
 
         if(regIp.test(inputbar)){ /* regIp와 input 값 비교 */
@@ -63,3 +38,41 @@ function printResult(inputbar){
         tc.disabled = false; /* 쓰레기통 모양 아이콘 활성화 */
     }
 }
+
+/* fetch로 좌상단 서버타임 출력하기 */
+// setInterval(function serverTime(){ 
+//     const config = {
+//         method: "get"
+//     }; /* GET request */
+//     fetch("http://worldtimeapi.org/api/timezone/Asia/Seoul", config) /* url 주소로 get 요청 보내기 */
+//         .then(response => response.json())    /* json 형식으로 응답받음 */
+//         .then(data => {
+//             const time = document.getElementById("time"); /* time 출력창 */
+//             var getTime = data.datetime;
+//             var setDay = getTime.substring(0,10); /* 전체 문자열 중 날짜에 해당 */
+//             var setTime = getTime.substring(11,19); /* 전체 문자열 중 시간에 해당 */
+//             time.innerText = setDay + " " + setTime; /* 최종 time 출력 */
+//     })
+//     .catch(error => console.log(error)); /* error 발생시 */
+// },1000);
+
+// ajax로 서버시간 호출하기
+setInterval(function request_time() {
+    $.ajax({
+        //Get 방식
+        type: "GET", 
+        url: "http://worldtimeapi.org/api/timezone/Asia/Seoul",
+        //호출 성공했을때
+        success: function(res){
+            const time = document.getElementById("time"); //time 출력창
+            var getTime = res.datetime;
+            var setDay = getTime.substring(0,10); // 전체 문자열 중 날짜에 해당
+            var setTime = getTime.substring(11,19); // 전체 문자열 중 시간에 해당
+            time.innerText = setDay + " " + setTime; // 최종 time 출력
+        },
+        //호출 error
+        error:  function(XMLHttpRequest, textStatus, errorThrown){
+            alert("Fail");
+        } 
+    })
+}, 1000); //1초마다 한번씩
