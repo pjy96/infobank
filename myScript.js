f14d8e4a6b8eefc4a41536196ab47920477e94cb
pjy96 <doooooo02@kakao.com>
2022-07-14 15:35:53 +0900

    come on

--- myScript.js
+++ myScript.js
@@ -4,15 +4,6 @@ function fClose(){
     del.value = ""; /* 검색바 내용 삭제 */
 }
 
-/* 쓰레기통 모양 아이콘 기능 : 출력 내용 삭제 */
-function rClose(){ 
-    var delt = document.getElementById('result');
-    var tc = document.getElementById("rClose"); /* 쓰레기통 모양 아이콘 */
- 
-    delt.innerHTML = ""; /* 출력 내용 삭제 */
-    tc.disabled = true; /* 아이콘 비활성화 */
-}
-
 /* after enter, run reg, print result */
 function printResult(inputbar){
 
@@ -26,54 +17,66 @@ function printResult(inputbar){
     /* Email 정규식 : \w(=[0-9a-zA-Z_]) 1회이상 @ \w 1회이상 "." 숫자&알파벳 */
     const regEmail = /^\w+@\w+\.[0-9a-zA-Z]+$/;
 
+    // if(window.event.keyCode == 13){ /* enter's ascii code number = 13 */
 
-    var addList = document.createElement("p"); // 새로운 태그를 마지막 자식노드로 추가
-    var textnode = document.createTextNode(element.innerText + " | " + inputbar); //텍스트 생성
+    //     if(regIp.test(inputbar)){ /* regIp와 input 값 비교 */
 
-    if(window.event.keyCode == 13){ /* enter's ascii code number = 13 */
+    //         result.innerText = element.innerText + " | " + inputbar + " is IP";
 
-        addList.appendChild(textnode);
-        result.innerText = addList.textContent;
+    //     }else if(regEmail.test(inputbar)){ /* regEmail과 input 값 비교 */
 
-        if(regIp.test(inputbar)){ /* regIp와 input 값 비교 */
+    //         result.innerText = element.innerText + " | " + inputbar + " is Email";
 
-            addList.append(element.innerText + " | " + inputbar + " is IP");
-            result.innerText = addList.textContent;
+    //     }else{ /* 유효하지 않은 형식입니다 */
 
-        }else if(regEmail.test(inputbar)){ /* regEmail과 input 값 비교 */
-            result.innerText = element.innerText + " | " + inputbar + " is Email";
-        }else{ /* 유효하지 않은 형식입니다 */
-            result.innerText = element.innerText + " | " + inputbar + " is invalid format";
-        }
+    //         result.innerText = element.innerText + " | " + inputbar + " is invalid format";
+    //     }
 
-        tc.disabled = false; /* 쓰레기통 모양 아이콘 활성화 */
+    //     tc.disabled = false; /* 쓰레기통 모양 아이콘 활성화 */
 
-    }
 }
 
+var arr = []; // 배열 생성
 function test(){
 
-    var element = document.getElementById("time"); /* div time (현재시간) */
-    var newDiv = document.createElement('div'); // div element 만들기
-    var newText = document.createTextNode(element.innerText); // div에 들어갈 textnode 만들기
+    // enter 입력시 배열에 저장하고 가장 최근에 저장된 배열을 가장 위에 출력
+    // foreach (각 요소 호출) -> var text += 시간 + " | " + inputbar + 검사결과 + "<br>";
+    // pop : 마지막 요소 제거
+    // unshift : (배열을 역순으로) 새 항목 추가
 
-    newDiv.appendChild(newText); // div에 textnode 붙이기
-    Container.appendChild(newDiv); // 결과 출력창에 div element 붙이기
-    console.log(element.innerText)
-    // 여기까진 element appendChild
+    // 1. enter 입력시 배열에 저장 
+    // 2. 배열의 길이는 5개로 제한 (5개를 초과하면 가장 오래된/마지막 데이터 삭제)
+    // 3. 가장 최근 데이터부터 출력 (배열 전체)
+    // + 가장 최근 데이터가 데이터의 가장 처음 인덱스에 위치해야함 
+    // 4. 배열의 길이가 0개 이상이면 
 
-    // var arr = [5]; // 배열선언
-    // var entire = arr.push(element); // 배열에 element 추가
-
-    // console.log(arr)
-    // console.log(entire)
+    var inputbar = document.getElementById("inputbar").value; // inputbar 값 가져오기
+    var result = document.getElementById("result"); // 검색결과 출력창
     
-    // 클릭수++로 5개까지 제한하고 삭제할때 remove --
+    arr.unshift(inputbar); // 배열에 저장(가장 최근 데이터가 위로 오게)
+    //배열의 길이가 5개를 넘지 않게
+    if(arr.length > 5){
+        arr.pop(); // 가장 오래된 데이터 삭제
+    }
+
+    // 화면에 출력
+    arr.forEach(element => {
+        console.log(element);
+        result.innerText = element + "\n";
+        result.innerText = arr
+    })
+
 
 }
 
-
-
+/* 쓰레기통 모양 아이콘 기능 : 출력 내용 삭제 */
+function rClose(){ 
+    var delt = document.getElementById('result');
+    var tc = document.getElementById("rClose"); /* 쓰레기통 모양 아이콘 */
+ 
+    delt.innerHTML = ""; /* 출력 내용 삭제 */
+    tc.disabled = true; /* 아이콘 비활성화 */
+}
 
 /* fetch로 좌상단 서버타임 출력하기 */
 // setInterval(function serverTime(){ 
