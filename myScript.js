902ecd0d7e01424a2d625f170c9d318c1e57e338
pjy96 <doooooo02@kakao.com>
2022-07-14 16:46:27 +0900

    go

--- myScript.js
+++ myScript.js
@@ -5,6 +5,7 @@ function fClose(){
 }
 
 /* after enter, run reg, print result */
+var arr = []; // 배열 생성
 function printResult(inputbar){
 
     var inputbar = document.getElementById("inputbar").value; /* 입력 받은 내용*/
@@ -17,28 +18,27 @@ function printResult(inputbar){
     /* Email 정규식 : \w(=[0-9a-zA-Z_]) 1회이상 @ \w 1회이상 "." 숫자&알파벳 */
     const regEmail = /^\w+@\w+\.[0-9a-zA-Z]+$/;
 
-    // if(window.event.keyCode == 13){ /* enter's ascii code number = 13 */
+    if(window.event.keyCode == 13){ /* enter's ascii code number = 13 */
 
-    //     if(regIp.test(inputbar)){ /* regIp와 input 값 비교 */
+        if(regIp.test(inputbar)){ /* regIp와 input 값 비교 */
 
-    //         result.innerText = element.innerText + " | " + inputbar + " is IP";
+            result.innerText = element.innerText + " | " + inputbar + " is IP";
 
-    //     }else if(regEmail.test(inputbar)){ /* regEmail과 input 값 비교 */
+        }else if(regEmail.test(inputbar)){ /* regEmail과 input 값 비교 */
 
-    //         result.innerText = element.innerText + " | " + inputbar + " is Email";
+            result.innerText = element.innerText + " | " + inputbar + " is Email";
 
-    //     }else{ /* 유효하지 않은 형식입니다 */
+        }else{ /* 유효하지 않은 형식입니다 */
 
-    //         result.innerText = element.innerText + " | " + inputbar + " is invalid format";
-    //     }
+            result.innerText = element.innerText + " | " + inputbar + " is invalid format";
+        }
 
-    //     tc.disabled = false; /* 쓰레기통 모양 아이콘 활성화 */
-
+        tc.disabled = false; /* 쓰레기통 모양 아이콘 활성화 */
+    }
 }
 
-var arr = []; // 배열 생성
 function test(){
-
+    
     // <배열 출력>
     // 1. enter 입력시 배열에 저장 
     // 2. 배열의 길이는 5개로 제한 (5개를 초과하면 가장 오래된/마지막 데이터 삭제)
@@ -47,21 +47,18 @@ function test(){
 
     var inputbar = document.getElementById("inputbar").value; // inputbar 값 가져오기
     var result = document.getElementById("result"); // 검색결과 출력창
-    var tc = document.getElementById("rClose"); /* 쓰레기통 모양 아이콘 */
 
+    result.innerText = ""; //출력창 초기화
     arr.unshift(inputbar); // 배열에 저장(가장 최근 데이터가 위로 오게)
     //배열의 길이가 5개를 넘지 않게
     if(arr.length > 5){
         arr.pop(); // 가장 오래된 데이터 삭제
     }
-
     // 화면에 출력
     arr.forEach(element => {
-        // result.innerText = '';
         result.innerText += element + "\n";
     })
-
-
+    
 }
 
 /* 쓰레기통 모양 아이콘 기능 : 출력 내용 삭제 */
