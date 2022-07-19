f9edf419b46b94d3f64eee083a1b598f7850f701
pjy96 <doooooo02@kakao.com>
2022-07-19 14:25:10 +0900

    success

--- myScript.js
+++ myScript.js
@@ -1,49 +1,20 @@
+var arr = []; // 배열생성 
+
 /* xmark function */
 function fClose(){
     var inputbar = document.getElementById('inputbar');
     inputbar.value = ""; /* 검색바 내용 삭제 */
 }
 
-/* after enter, run reg, print result */
-var arr = []; // 배열생성
+/* Enter 입력 -> 유효성 검사 -> 결과 출력 */
 function printResult(inputbar){
-
     var inputbar = document.getElementById("inputbar").value; /* 입력 받은 내용*/
-    var result = document.getElementById("result"); /* 검색결과 출력창*/
     //var tc = document.getElementById("rClose"); /* 삭제 버튼 */
-    var element = document.getElementById("time"); /* div time (현재시간) */
 
     /* IP 정규식 : 0~255사이의 숫자 + "." + 0~255사이의 숫자 + "." + 0~255사이의 숫자 + "." 0~255사이의 숫자 */
     const regIp = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/; 
     /* Email 정규식 : \w(=[0-9a-zA-Z_]) 1회이상 @ \w 1회이상 "." 숫자&알파벳 */
     const regEmail = /^\w+@\w+\.[0-9a-zA-Z]+$/;
-
-    const delButton = document.createElement("input");
-    delButton.type = "button";
-    delButton.innerHTML = "X";
-    delButton.style.color = "red";
-    delButton.onclick = function(){
-        result.innerHTML = "";
-        tc.disabled = true;
-    }
-
-
-
-    function printArray(resultString){ // IP 출력
-        result.innerText = ""; // 출력창 초기화
-        tst = element.innerText + " | " + inputbar + resultString; // 저장할 문자열
-        arr.unshift(tst); // 배열에 저장(가장 최근 데이터가 위로 오게)
-        for(var i=0; i<arr.length; i++){ // 배열길이만큼 배열 출력
-            result.innerHTML += arr[i]  + "<br>";
-            // result.innerHTML += arr[i];
-            // result.innerHTML += delButton;
-            // result.innerHTML += "<br>";
-            delButton;
-        }
-        if(arr.length >= 5){ // 배열의 길이가 5를 넘지 않게
-            arr.pop(); // 가장 오래된 데이터 삭제
-        }
-    }
 
     if(window.event.keyCode == 13){ /* Enter 입력 시 */
         if(regIp.test(inputbar)){ /* regIp */
@@ -55,18 +26,35 @@ function printResult(inputbar){
         }
         //tc.disabled = false; /* 쓰레기통 모양 아이콘 활성화 */
     }
-
-    // const rClose = document.querySelectorAll(".rClose"); // 삭제버튼 선택
-    // rClose.forEach(btn => {
-    //     btn.addEventListener("click", (event) => {
-    //         result.innerHTML = "";
-    //         tc.disabled = true; 
-    //     })
-    //     //slice
-    // })
-
 }
 
+/* 입력받은 값 배열에 저장 -> 양식에 맞게 출력 */
+function printArray(resultString){
+
+    var inputbar = document.getElementById("inputbar").value; /* 입력 받은 내용*/
+    var result = document.getElementById("result"); /* 검색결과 출력창*/
+    var element = document.getElementById("time"); /* div time (현재시간) */
+
+    result.innerHTML = ""; // 출력창 초기화
+    tst = element.innerHTML + " | " + inputbar + resultString; // 저장할 문자열
+
+    arr.unshift(tst); // 문자열을 배열에 저장(가장 최근 데이터가 위로 오게)
+    if(arr.length > 5){ // 배열의 길이가 5를 넘지 않게
+        arr.pop(); // 가장 오래된 데이터 삭제
+    }
+    for(var i=0; i<arr.length; i++){
+        result.innerHTML += arr[i] +"<button onclick=\"deleteArray("+i+")\">삭제</button><br>";
+    }
+}
+
+/* 삭제버튼 function */
+function deleteArray(idx){
+    arr.splice(idx,1); // 배열의 idx번째 내용 삭제
+    result.innerHTML = ""; // 출력창 초기화
+    for(var i=0; i<arr.length; i++){ // 재출력
+        result.innerHTML += arr[i] +"<button onclick=\"deleteArray("+i+")\">삭제</button><br>";
+    }
+}
 
 // ajax로 서버시간 호출하기
 setInterval(function request_time() {
